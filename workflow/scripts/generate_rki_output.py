a6fc415c568e0ae49b7883792edefbedfb00adea
alethomas <alex.thomas89@gmail.com>
2021-02-24 10:51:41 +0000

    edit to new format

--- workflow/scripts/generate_rki_output.py
+++ workflow/scripts/generate_rki_output.py
@@ -24,21 +24,12 @@ for file in snakemake.input:
 
 outtab = open(snakemake.output.table, "a")
 outfile = open(snakemake.output.fasta, "a")
-
 if os.path.getsize(snakemake.output.table) == 0:
-        outtab.write("IMS_ID;SENDING_LAB;DATE_DRAW;SEQ_TYPE;SEQ_REASON;SAMPLE_TYPE;OWN_FASTA_ID\n")
-countlist = open("resources/countlist.txt", "r")
-for line in countlist:
-        counter = int(line.split(",")[1]) # get current start for numbering
-countlist.close()
-countlist = open("resources/countlist.txt", "a")
+        outtab.write("SENDING_LAB;DATE_DRAW;SEQ_TYPE;SEQ_REASON;SAMPLE_TYPE;PUBLICATION_STATUS;OWN_FASTA_ID\n")
 for key in header2seq:
         print(key)
         if len(header2seq[key]) > min_length:
-                counter += 1
-                outtab.write("IMS-10259-CVDP-%05d;IMS-10259-CVDP-%05d;;ILLUMINA;N;s001;%s\n" % (counter, counter, key[1:]))
-                outfile.write(key + "\n" + header2seq[key] + "\n")
-                countlist.write(key[1:] + "," + str(counter) + "\n")
-                
+                outtab.write("10259;;ILLUMINA;N;s001;N;%s\n" % (key[1:]))
+                outfile.write(key + "\n" + header2seq[key] + "\n") 
 outfile.close()
 outtab.close()
