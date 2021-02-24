ab3d0bc9d3dd905a87679c56e7374395961f0a4b
alethomas <alex.thomas89@gmail.com>
2021-02-24 14:59:36 +0000

    integrate script for extracting rof

--- workflow/scripts/extract-reads-of-interest.py
+++ workflow/scripts/extract-reads-of-interest.py
@@ -4,7 +4,7 @@ sars_cov2_id = "NC_045512"
 
 def is_sars_cov2(record, mate=False):
     if mate:
-        return record.next_reference_name.startwith(sars_cov2_id)
+        return record.next_reference_name.startswith(sars_cov2_id)
     else:
         return record.reference_name.startswith(sars_cov2_id)
 
@@ -13,8 +13,8 @@ with pysam.AlignmentFile(snakemake.input[0], "rb") as inbam:
         for record in inbam:
             assert record.is_paired
             if (
-                (record.is_unmapped and record.is_mate_unmapped) or
-                (is_sars_cov2(record) and record.is_mate_unmapped) or 
+                (record.is_unmapped and record.mate_is_unmapped) or
+                (is_sars_cov2(record) and record.mate_is_unmapped) or 
                 (is_sars_cov2(record, mate=True) and record.is_unmapped) or 
                 (is_sars_cov2(record) and is_sars_cov2(record, mate=True))
             ):
