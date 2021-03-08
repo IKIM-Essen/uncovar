896d4bf15541d509c485536e9e92582ee331bd8b
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-03-08 11:15:18 +0000

    update coverage plot

--- workflow/scripts/plot-all-coverage.py
+++ workflow/scripts/plot-all-coverage.py
@@ -1,34 +1,39 @@
-sys.stderr = open(snakemake.log[0], "w")
-min_coverage = snakemake.params.get("min_coverage", "")
-
 import pandas as pd
 import altair as alt
 
 
 def plot_coverage(sm_input, sm_output, min_coverage):
-    coverage = pd.read_csv(sm_input, sep="\t")
 
-    coverage.rename(
-        columns={coverage.columns[2]: "Coverage", "POS": "Pos"}, inplace=True
-    )
+    coverage = pd.DataFrame()
+    for sample in sm_input:
+        sample_df = pd.read_csv(sample, sep="\t")
 
-    coverage["Sample"] = coverage["#CHROM"].apply(lambda x: str(x).split(".")[0])
+        sample_df.rename(
+            columns={sample_df.columns[2]: "Coverage", "POS": "Pos"}, inplace=True
+        )
+
+        sample_df["Sample"] = sample_df["#CHROM"].apply(lambda x: str(x).split(".")[0])
+
+        coverage= coverage.append(sample_df, ignore_index=True)
+
     coverage["# Coverage"] = coverage.Coverage.apply(
         lambda x: f"< {min_coverage}"
         if int(x) < int(min_coverage)
         else f">= {min_coverage}"
     )
 
-    # max_x_pos = 30000 if coverage.Pos.max() < 30000 else coverage.Pos.max()
+    max_y_pos = 500
     max_x_pos = coverage.Pos.max()
+
+    coverage["Coverage"] = coverage["Coverage"].apply(lambda x: max_y_pos if x>max_y_pos else x)
 
     if len(coverage) > 0:
         alt.Chart(coverage).mark_bar().encode(
             x=alt.X("Pos:Q", scale=alt.Scale(domain=(0, max_x_pos), nice=False)),
             y=alt.Y(
-                "Coverage", scale=alt.Scale(domain=[0, coverage.Coverage.max() * 1.05])
+                "Coverage", scale=alt.Scale(domain=[0, max_y_pos])
             ),
-            column=alt.Column("Sample:N"),
+            row=alt.Row("Sample:N"),
             color=alt.Color(
                 "# Coverage",
                 scale=alt.Scale(
@@ -43,4 +48,7 @@ def plot_coverage(sm_input, sm_output, min_coverage):
         )
 
 
-plot_coverage(snakemake.input[0], snakemake.output[0], min_coverage)
+sys.stderr = open(snakemake.log[0], "w")
+min_coverage = snakemake.params.get("min_coverage", "")
+
+plot_coverage(snakemake.input, snakemake.output[0], min_coverage)
