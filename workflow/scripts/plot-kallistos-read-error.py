91e681641e2780363139d3fabd11f8ed8fca0c97
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-03-16 15:31:39 +0000

    add heatmap

--- workflow/scripts/plot-kallistos-read-error.py
+++ workflow/scripts/plot-kallistos-read-error.py
@@ -18,17 +18,25 @@ def mask(string):
         return "some strain"
 
 
-def plot_error(sm_input, sm_output):
+def plot_error(sm_input, sm_output, type="heatmap"):
     results_df = pd.read_csv(sm_input, delimiter="\t")
 
     results_df["Kallisto output"] = results_df["target_id"].apply(lambda x: mask(x))
     no_of_mixs = int(results_df["mix"].max() + 1)
+    
+    if type=="heatmap":
+        plot = alt.Chart(results_df).mark_rect().encode(
+        alt.X('true_fraction:Q', bin=alt.Bin(maxbins=20)),
+        alt.Y('est_fraction:Q', bin=alt.Bin(maxbins=20)),
+        alt.Color('count(true_fraction):Q', scale=alt.Scale(scheme='greenblue'))
+        )
 
-    scatter = (
-        alt.Chart(results_df)
-        .mark_point()
-        .encode(x="true_fraction:Q", y="est_fraction:Q", color="Kallisto output:N")
-    )
+    else: 
+        plot = (
+            alt.Chart(results_df)
+            .mark_point()
+            .encode(x="true_fraction:Q", y="est_fraction:Q", color="Kallisto output:N")
+        )
 
     line = (
         alt.Chart(pd.DataFrame({"true_fraction": [0, 1], "est_fraction": [0, 1]}))
@@ -36,7 +44,7 @@ def plot_error(sm_input, sm_output):
         .encode(alt.X("true_fraction"), alt.Y("est_fraction"))
     )
 
-    (scatter + line).properties(title=f"No. of mixtures {no_of_mixs}").save(sm_output)
+    (plot + line).properties(title=f"No. of mixtures {no_of_mixs}").save(sm_output)
 
 
 if __name__ == "__main__":
