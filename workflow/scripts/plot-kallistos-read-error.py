193d86cbe0d9d16e6f4b75e060e15dfbd71f9a2b
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-03-17 11:10:09 +0000

    fmt

--- workflow/scripts/plot-kallistos-read-error.py
+++ workflow/scripts/plot-kallistos-read-error.py
@@ -5,6 +5,7 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
+
 def mask(string):
     if string == "other":
         return "other"
@@ -23,37 +24,51 @@ def plot_error(sm_input, sm_output, type="heatmap"):
 
     results_df["Kallisto output"] = results_df["target_id"].apply(lambda x: mask(x))
     no_of_mixs = int(results_df["mix"].max() + 1)
-    
-    if type=="heatmap":
-        plot = alt.Chart(results_df).mark_rect().encode(
-            alt.X(
-                'true_fraction:Q', 
-                axis=alt.Axis(format='%', title='True Fraction'),
-                bin=alt.Bin(maxbins=49), 
-                scale=alt.Scale(bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])),
-            alt.Y(
-                'est_fraction:Q', 
-                axis=alt.Axis(format='%', title='Est. Fraction'),
-                bin=alt.Bin(maxbins=50), 
-                scale=alt.Scale(bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])),
-            alt.Color('count(true_fraction):Q', scale=alt.Scale(type='log', scheme='greenblue'))
+
+    if type == "heatmap":
+        plot = (
+            alt.Chart(results_df)
+            .mark_rect()
+            .encode(
+                alt.X(
+                    "true_fraction:Q",
+                    axis=alt.Axis(format="%", title="True Fraction"),
+                    bin=alt.Bin(maxbins=49),
+                    scale=alt.Scale(
+                        bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+                    ),
+                ),
+                alt.Y(
+                    "est_fraction:Q",
+                    axis=alt.Axis(format="%", title="Est. Fraction"),
+                    bin=alt.Bin(maxbins=50),
+                    scale=alt.Scale(
+                        bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+                    ),
+                ),
+                alt.Color(
+                    "count(true_fraction):Q",
+                    scale=alt.Scale(type="log", scheme="greenblue"),
+                ),
+            )
         )
 
         # Define the degree of the polynomial fits
         degree_list = [1, 3]
 
-        base = alt.Chart(results_df).mark_circle().encode(
-            alt.X("true_fraction"), 
-            alt.Y("est_fraction")
+        base = (
+            alt.Chart(results_df)
+            .mark_circle()
+            .encode(alt.X("true_fraction"), alt.Y("est_fraction"))
         )
 
         polynomial_fit = [
             base.transform_regression(
-                "true_fraction", 
-                "est_fraction", 
-                method="poly", 
-                order=order, 
-                as_=["true_fraction", str(order)]
+                "true_fraction",
+                "est_fraction",
+                method="poly",
+                order=order,
+                as_=["true_fraction", str(order)],
             )
             .mark_line(color="black")
             .transform_fold([str(order)], as_=["Poly. Degree", "est_fraction"])
@@ -63,7 +78,7 @@ def plot_error(sm_input, sm_output, type="heatmap"):
 
         plot = alt.layer(plot, *polynomial_fit)
 
-    else: 
+    else:
         plot = (
             alt.Chart(results_df)
             .mark_point()
@@ -72,12 +87,11 @@ def plot_error(sm_input, sm_output, type="heatmap"):
 
     line = (
         alt.Chart(pd.DataFrame({"true_fraction": [0, 1], "est_fraction": [0, 1]}))
-        .mark_line(color='grey')
+        .mark_line(color="grey")
         .encode(alt.X("true_fraction"), alt.Y("est_fraction"))
     )
 
-    (plot + line ).properties(title=f"No. of mixtures {no_of_mixs}").save(sm_output)
-
+    (plot + line).properties(title=f"No. of mixtures {no_of_mixs}").save(sm_output)
 
 
 def plot_zeros(sm_input, sm_output):
@@ -85,21 +99,23 @@ def plot_zeros(sm_input, sm_output):
 
     results_df["Kallisto output"] = results_df["target_id"].apply(lambda x: mask(x))
 
-    results_df = results_df[(results_df["true_fraction"] == 0) | (results_df["est_fraction"] == 0)]
+    results_df = results_df[
+        (results_df["true_fraction"] == 0) | (results_df["est_fraction"] == 0)
+    ]
 
-    results_df["Axis"] = results_df["true_fraction"].apply(lambda x: "True Fraction = 0" if x == 0 else "Est. Fraction = 0")
-    
+    results_df["Axis"] = results_df["true_fraction"].apply(
+        lambda x: "True Fraction = 0" if x == 0 else "Est. Fraction = 0"
+    )
+
     base = alt.Chart(results_df[["true_fraction", "target_id", "Axis"]])
 
-    bar = base.mark_bar().encode(
-        x=alt.X('target_id', sort='y'),
-        y=alt.Y('count(target_id)')
-    ).facet(
-        row='Axis:N',
+    bar = (
+        base.mark_bar()
+        .encode(x=alt.X("target_id", sort="y"), y=alt.Y("count(target_id)"))
+        .facet(row="Axis:N",)
     )
 
     (bar).save(sm_output)
-
 
 
 if __name__ == "__main__":
