d320d4ed7f53ffff666dc5661b18d3fff1fa4e47
Felix Wiegand <fxwiegand@gmail.com>
2021-04-07 19:02:04 +0200

    Remove wrong pop() call

--- workflow/scripts/generate_virologist_output.py
+++ workflow/scripts/generate_virologist_output.py
@@ -213,7 +213,7 @@ for sample in table:
 
 """Iterate variants of interest and other variants and check if gene and alteration are contained in pangolin data."""
 for sample in table:
-    lineage = table[sample][0].pop().lower()
+    lineage = table[sample][0][0].lower()
     print(f"Checking lineage: {lineage}")
     if lineage in pangolin_lineages:
         for variant in itertools.chain(table[sample][1], table[sample][2]):
