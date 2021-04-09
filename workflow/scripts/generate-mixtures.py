5486842295b3445cd887ba9418a1cb9c30707bcf
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-04-09 07:19:52 +0000

    fix typo

--- workflow/scripts/generate-mixtures.py
+++ workflow/scripts/generate-mixtures.py
@@ -1,5 +1,5 @@
 sys.stderr = open(snakemake.log[0], "w")
 
-with open(snakemakeoutput[0], "w") as out:
+with open(snakemake.output[0], "w") as out:
     for mix in snakemake.params.mixtures:
         out.write(f"{mix}\n")
