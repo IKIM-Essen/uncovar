fbcc81d4a63b6f2e5f5d6c3b45f8cd4b81bd1c77
alethomas <alex.thomas89gmail.com>
2021-04-26 08:05:58 +0000

    switch to snakemake shell

--- workflow/scripts/ucsc_vcf.py
+++ workflow/scripts/ucsc_vcf.py
@@ -1,10 +1,10 @@
-import subprocess
 import sys
 import pandas as pd
+from snakemake.shell import shell
 
 sys.stderr = open(snakemake.log[0], "w")
 
 pangolin_results = pd.read_csv(snakemake.input.strain_call)
 strain = pangolin_results.loc[0]["lineage"]
 
-subprocess.call("bcftools view -Ov %s | (echo track name=%s description=%s-%s; cat -) > %s" % (snakemake.input.bcfs, snakemake.wildcards.target, strain, snakemake.wildcards.filter, snakemake.output), shell=True)
+shell("bcftools view -Ov {snakemake.input.bcfs} | (echo track name={snakemake.wildcards.target} description={strain}-{snakemake.wildcards.filter}; cat -) > {snakemake.output}")
