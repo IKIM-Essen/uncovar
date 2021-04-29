423f94013058e67f0864ca9c0dab9aa9eda0a7d5
Alexander Thomas <77535027+alethomas@users.noreply.github.com>
2021-04-29 13:06:36 +0200

    Fix dry run by moving shell command into script (#121)
    * add bcftools
    
    * switch to script instead of shell
    
    * script to fix dry run
    
    * fmt
    
    * updated rbt version
    
    * change rbt version
    
    * switch to snakemake shell
    
    * add dryrun test flag
    
    * add if statement for report test
    
    * fmt
    
    Co-authored-by: alethomas <alex.thomas89gmail.com>
    Co-authored-by: Johannes Köster <johannes.koester@tu-dortmund.de>

---
+++ workflow/scripts/ucsc_vcf.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+from snakemake.shell import shell
+
+sys.stderr = open(snakemake.log[0], "w")
+
+pangolin_results = pd.read_csv(snakemake.input.strain_call)
+strain = pangolin_results.loc[0]["lineage"]
+
+shell("bcftools view -Ov {snakemake.input.bcfs} | (echo track name={snakemake.wildcards.target} description={strain}-{snakemake.wildcards.filter}; cat -) > {snakemake.output}")
