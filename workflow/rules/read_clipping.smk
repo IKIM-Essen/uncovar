671a5b6be1dfaefdc45ab0b69d07024e8e0aa27b
alethomas <alex.thomas89@gmail.com>
2021-05-18 16:01:09 +0200

    enable plotting

--- workflow/rules/read_clipping.smk
+++ workflow/rules/read_clipping.smk
@@ -10,7 +10,6 @@ rule clip_primer:
         ),
     output:
         sortbam=temp("results/{date}/clipped-reads/{sample}.bam"),
-        sortindex=temp("results/{date}/clipped-reads/{sample}.bam.bai"),
         clippedbam=temp("results/{date}/clipped-reads/{sample}.primerclipped.bam"),
         hardclippedbam=temp(
             "results/{date}/clipped-reads/{sample}.primerclipped.hard.bam"
@@ -34,7 +33,7 @@ rule clip_primer:
     shell:
         """
         samtools sort -@ {threads} -o {output.sortbam} {input.bam} > {log} 2>&1
-        samtools index {output.sortbam} >> {log} 2>&1
+        
         cd {params.dir}
         bamclipper.sh -b {params.bam} -p {params.dir_depth}{input.bed} -n {threads} >> {params.dir_depth}{log} 2>&1
         cd {params.dir_depth}
@@ -42,3 +41,57 @@ rule clip_primer:
         samtools sort  -@ {threads} -n {output.hardclippedbam} -o {output.sorthardclippedbam}  >> {log} 2>&1
         samtools fastq -@ {threads} {output.sorthardclippedbam} -1 {output.fq1} -2 {output.fq2}  >> {log} 2>&1
         """
+
+
+rule clp_aln:
+    input:
+        reads=[
+            "results/{date}/clipped-reads/{sample}.1.fastq.gz",
+            "results/{date}/clipped-reads/{sample}.2.fastq.gz",
+        ],
+        idx=get_bwa_index,
+    output:
+        "results/{date}/clipped-aln/ref~{reference}/{sample}.bam",
+    log:
+        "logs/{date}/clipped-aln/ref~{reference}/{sample}.log",
+    params:
+        index=lambda w, input: os.path.splitext(input.idx[0])[0],
+        extra="",
+        sort="samtools",
+        sort_order="coordinate",
+    threads: 8
+    wrapper:
+        "0.69.0/bio/bwa/mem"
+
+
+rule sort_aln_for_plots:
+    input:
+        "results/{date}/clipped-reads/{sample}.primerclipped.hard.bam",
+    output:
+        "results/{date}/clipped-reads/{sample}.primerclipped.hard.c_sort.bam",
+    log:
+        "logs/{date}/sort-aln-for-plots/{sample}.log",
+    conda:
+        "../envs/samtools.yaml"
+    shell:
+        "samtools sort -o {output} {input} > {log} 2>&1"
+    
+
+
+rule plot_primer_clipping:
+    input:
+        unclipped=lambda wildcards: expand("results/{{date}}/clipped-reads/{sample}.bam", sample=get_samples_for_date(wildcards.date)),
+        index_unclipped=lambda wildcards: expand("results/{{date}}/clipped-reads/{sample}.bam.bai", sample=get_samples_for_date(wildcards.date)),
+        clipped=lambda wildcards: expand("results/{{date}}/clipped-reads/{sample}.primerclipped.hard.c_sort.bam", sample=get_samples_for_date(wildcards.date)),
+        index_clipped=lambda wildcards: expand("results/{{date}}/clipped-reads/{sample}.primerclipped.hard.c_sort.bam.bai", sample=get_samples_for_date(wildcards.date)),
+    output:
+        plot="results/{date}/plots/all.svg",
+    log:
+        "logs/{date}/plot-primer-clipping/{date}.log",
+    threads: 16
+    params:
+        samples=lambda wildcards: get_samples_for_date(wildcards.date),
+    conda:
+        "../envs/python.yaml"
+    script:
+        "../scripts/plot-primer-clipping.py"
--- workflow/scripts/plot-primer-clipping.py
+++ workflow/scripts/plot-primer-clipping.py
@@ -1,11 +1,10 @@
-from numpy.core.numeric import base_repr
 import pandas as pd
 import pysam
 from intervaltree import Interval, IntervalTree
 import altair as alt
 
 # read primer bedpe to df
-PRIMER = pd.read_csv("/local/data/alex/testing/snakemake-workflow-sars-cov2/resources/primer.bedpe", delimiter="\t", header=None)
+PRIMER = pd.read_csv("resources/primer.bedpe", delimiter="\t", header=None)
 PRIMER.drop(PRIMER.columns[[0, 3]], axis=1, inplace=True)
 PRIMER.columns = ['p1_start', 'p1_end', 'p2_start', 'p2_end']
 
@@ -17,7 +16,11 @@ for index, row in PRIMER.iterrows():
     no_primer_intervals[row['p1_end']+1: row['p2_start']] = (row['p1_end']+1, row['p2_start'])
 
 
-def count_intervals(file, state):
+def iter_with_samples(inputfiles):
+    return zip(snakemake.params.samples, inputfiles)
+
+
+def count_intervals(file):
     with pysam.AlignmentFile(file, "rb") as bam:
         counter_primer = 0
         counter_no_primer = 0
@@ -31,7 +34,7 @@ def count_intervals(file, state):
             else:
                 mate_pair_intervals[read.query_name].append(read.reference_end)
         for pair in mate_pair_intervals:
-            if mate_pair_intervals[pair][0] != None and mate_pair_intervals[pair][1] != None:
+            if len(mate_pair_intervals[pair]) >1 and mate_pair_intervals[pair][0] != None and mate_pair_intervals[pair][1] != None:
                 if primer_intervals.envelop(mate_pair_intervals[pair][0], mate_pair_intervals[pair][1]+1):
                     if sorted(primer_intervals.envelop(mate_pair_intervals[pair][0], mate_pair_intervals[pair][1]+1))[0].begin == mate_pair_intervals[pair][0] \
                     and sorted(primer_intervals.envelop(mate_pair_intervals[pair][0], mate_pair_intervals[pair][1]+1))[0].end == mate_pair_intervals[pair][1]+1:
@@ -48,28 +51,41 @@ def count_intervals(file, state):
                     counter_nothing += 1
             else:
                 counter_nothing += 1
-        counters = pd.DataFrame({'n_count': [counter_primer, counter_primer_within, counter_no_primer, counter_no_primer_within, counter_nothing], state: ["w/ primer", "w/ primer within", "w/o primer", "w/o primer within", "none"]})
+        counters = pd.DataFrame({'n_count': [counter_primer, counter_primer_within, counter_no_primer, counter_no_primer_within, counter_nothing], "class": ["w/ primer", "w/ primer within", "w/o primer", "w/o primer within", "none"]})
         return counters
 
 
 def plot_classes(counters, state):
     bars = alt.Chart(counters).mark_bar().encode(
     # y=alt.Y("class", title=""),
-    y=state,
-    x='n_count',)
+    y="class",
+    x='n_count',
+    row=alt.Row("sample:N"),
+    column=alt.Column("state:N", sort="descending")
+    )
     text = bars.mark_text(
     align='left',
     baseline='middle',
     dx=3  # Nudges text to right so it doesn't appear on top of the bar
     ).encode(
-    text='n_count'
+    text='n_count',
+    row=alt.Row("sample:N"),
+    column=alt.Column("state:N")
     ) 
     return bars, text
 
+all_df = pd.DataFrame()
+for sample, file in iter_with_samples(snakemake.input.unclipped):
+    counts_before = count_intervals(file)
+    counts_before["sample"] = sample
+    counts_before["state"] = "before"
+    all_df = all_df.append(counts_before, ignore_index=True)
 
-counts_before = count_intervals("/local/data/alex/testing/snakemake-workflow-sars-cov2/results/2021-04-30/clipped-reads/57815.bam", "before")
-bars_before, text_before = plot_classes(counts_before, "before")
-counts_after = count_intervals("/local/data/alex/testing/snakemake-workflow-sars-cov2/results/2021-04-30/clipped-reads/57815.primerclipped.hard.bam", "after")
-bars_after, text_after = plot_classes(counts_after, "after")
+for sample, file in iter_with_samples(snakemake.input.clipped):
+    counts_after = count_intervals(file)
+    counts_after["sample"] = sample
+    counts_after["state"] = "after"
+    all_df = all_df.append(counts_after, ignore_index=True)
+bars, text = plot_classes(all_df, "after")
 
-(bars_before + text_before & bars_after + text_after).properties(title='Amplicon matching').save("test.svg")
+(bars).properties(title='Amplicon matching').save(snakemake.output.plot)
