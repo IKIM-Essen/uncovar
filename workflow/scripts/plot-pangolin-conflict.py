5e3bc104aa88021b90fbd943593ad6a781663b35
Thomas Battenfeld <46334240+thomasbtf@users.noreply.github.com>
2021-07-10 17:05:29 +0200

    Add comments

--- workflow/scripts/plot-pangolin-conflict.py
+++ workflow/scripts/plot-pangolin-conflict.py
@@ -15,13 +15,15 @@ def plot_pangolin_conflict(sm_input, sm_output):
     # aggregate pangolin outputs
     all_sampes = pd.DataFrame()
     for input in sm_input:
-        # get
+        # get actual lineage
         _prefix, true_lineage_with_percent = input.split(MIXTURE_PART_INDICATOR)
         true_lineage, percent = true_lineage_with_percent.split(
             MIXTURE_PERCENTAGE_INDICATOR
         )
         true_lineage = true_lineage.replace("-", ".")
         percent = percent.replace(".strains.pangolin.csv", "")
+        
+        # create df for one call
         pangolin_output = pd.read_csv(input)
         pangolin_output["true_lineage"] = true_lineage
         pangolin_output["true_lineage_percent"] = percent
@@ -31,14 +33,15 @@ def plot_pangolin_conflict(sm_input, sm_output):
         all_sampes["lineage"] == all_sampes["true_lineage"]
     )
 
+    # get share of correct and incorrect calls
     print(
         all_sampes["correct_lineage_assignment"].value_counts(normalize=True),
         file=sys.stderr,
     )
 
     wrongly_assigned = all_sampes[all_sampes["correct_lineage_assignment"] == False]
-    print(wrongly_assigned)
 
+    # plot
     source = wrongly_assigned.copy()
     source.rename(
         columns={
