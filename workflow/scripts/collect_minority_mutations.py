883cbb6d15f696030b9d7be944223452e0c66734
ka-block <katharina.block@uni-due.de>
2022-10-26 10:19:45 +0000

    added minority and resistogram scripts with added License

---
+++ workflow/scripts/collect_minority_mutations.py
@@ -0,0 +1,237 @@
+# Copyright 2022 Thomas Battenfeld, Alexander Thomas, Johannes Köster.
+# Licensed under the BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+# This file may not be copied, modified, or distributed
+# except according to those terms.
+
+# import required packages
+import pathlib as path
+import os
+from xml.dom import INDEX_SIZE_ERR
+import pandas as pd
+import pysam
+import re
+import numpy as np
+import requests
+import glob
+
+# transfrom phred score to probability
+def phred_to_prob(phred):
+    if phred is None:
+        return pd.NA
+    return 10 ** (-phred / 10)
+
+# amino acid alphabet to translate 3 to 1 letter code
+AA_ALPHABET_TRANSLATION = {
+    "Gly": "G",
+    "Ala": "A",
+    "Leu": "L",
+    "Met": "M",
+    "Phe": "F",
+    "Trp": "W",
+    "Lys": "K",
+    "Gln": "Q",
+    "Glu": "E",
+    "Ser": "S",
+    "Pro": "P",
+    "Val": "V",
+    "Ile": "I",
+    "Cys": "C",
+    "Tyr": "Y",
+    "His": "H",
+    "Arg": "R",
+    "Asn": "N",
+    "Asp": "D",
+    "Thr": "T",
+}
+
+# create empty lists
+sig=[]
+rd=[]
+freq=[]
+prob=[]
+name=[]
+ddir=[]
+gen=[]
+posi=[]
+lin=[]
+
+# iterate over files in directory
+for file in glob.glob('/groups/ds/kblock/virusrecombination/results_15orfs/week*/annotated-calls/ref~main/annot~orf/*.bcf'):
+    bcf_in = (pysam.VariantFile(file))
+    # extract week name to append to dataframe
+    dir_name = os.path.split(os.path.split(os.path.split(os.path.split(os.path.dirname(file))[0])[0])[0])[1]
+    filename = path.Path(file).stem
+    # retrives pangolin lineage file
+    lin_dir = os.path.join('/groups/ds/kblock/virusrecombination/results_15orfs/', dir_name,'tables/pangolin_calls_per_stage.csv')
+    if os.path.exists(lin_dir):
+        lineages = pd.read_csv(lin_dir, dtype={'Sample': str})
+        idx = list(lineages['Sample']).index(str(filename))
+        # pseudo or scaffold(prefered) from pangolin_calls_per_stage.csv if scaff empty take pseudo
+        #ADD FILTER FOR UNASSIGNED
+        lineage = lineages['Scaffolded Seq'].values[idx] if lineages['Scaffolded Seq'].values[idx] != "Not called on" else lineages['Pseudo Seq'].values[idx]
+        # fill lists with data obtained from file
+        # iterate over records
+        for record in bcf_in:
+            pattern_count = str(record).count(':p.')
+            # iterate over annotations in record
+            # as there are multiple annotations per line, which cause problems, check for pattern (Aminoacid/protein annotation) first
+            for ann in record.info["ANN"]:
+                # split string seperated by pipe
+                anno = ann.split("|")
+                # get location
+                loc = anno[3]
+                # get feature_id
+                fea_id = anno[6]
+                # get alteration
+                alt = anno[11]
+                # check for occurence of pattern
+                if pattern_count == 0 and alt != "":
+                    continue
+                elif pattern_count != 0 and alt == "":
+                    continue
+                elif pattern_count != 0 and alt != "":
+                    regex=re.compile(r':p.')
+                    alteration = regex.split(alt)[1]
+                    # replace %3D and triplet aa code with one letter code
+                    for triplet, amino in AA_ALPHABET_TRANSLATION.items():
+                        alteration = alteration.replace("%3D", triplet[0]).replace(triplet, amino)
+                    alt = f"{loc}:{alteration}"
+                    pos = (record.pos)
+                    gene_name = loc
+                elif pattern_count == 0 and alt == "":
+                    # if regular name is not available annotate using nucleotides
+                    ref=(record.ref)
+                    pos=(record.pos)
+                    # if clause to find mutation on ORFs
+                    if 266 <= pos <= 13468:
+                        gene_name="ORF1a"
+                    elif 13468 <= pos <= 21555:
+                        gene_name="ORF1b"
+                    elif 21563 <= pos <= 25384:
+                        gene_name="S"
+                    elif 25393 <= pos <= 26220:
+                        gene_name="ORF3a"
+                    elif 25765 <= pos <= 26220:
+                        gene_name="ORF3b"
+                    elif 26245 <= pos <= 26472:
+                        gene_name="E"
+                    elif 26523 <= pos <= 27191:
+                        gene_name="M"
+                    elif 27202 <= pos <= 27387:
+                        gene_name="ORF6"
+                    elif 27394 <= pos <= 27759:
+                        gene_name="ORF7a"
+                    elif 27756 <= pos <= 27887:
+                        gene_name="ORF7b"
+                    elif 27894 <= pos <= 28259:
+                        gene_name="ORF8"
+                    elif 28284 <= pos <= 28577:
+                        gene_name="ORF9a"
+                    elif 28734 <= pos <= 28955:
+                        gene_name="ORF9b"
+                    elif 28274 <= pos <= 29533:
+                        gene_name="N"
+                    elif 29558 <= pos <= 29674:
+                        gene_name="ORF10"
+                    else:
+                        gene_name="none"
+                    alts=(record.alts[0])
+                    alt=f"{ref}{pos}{alts}"
+                else:
+                    raise Exception("Something unexpected occured in this record:", str(record), file)
+                # add results in lists
+                sig.append(alt)
+                # position
+                posi.append(pos)
+                # ORF
+                gen.append(gene_name)
+                # read depth
+                rd.append(record.samples[0]["DP"])
+                # frequency
+                freq.append(record.samples[0]["AF"][0])
+                # probability
+                prob.append(1.0 - (phred_to_prob(record.info["PROB_ABSENT"][0]) + phred_to_prob(record.info["PROB_ARTIFACT"][0])))
+                # filename
+                name.append(filename)
+                # week
+                ddir.append(dir_name)
+                # lineage
+                lin.append(lineage)
+    else:
+        print(dir_name, filename, "does not exists!")
+        continue
+    
+
+# create dataframe
+df= pd.DataFrame({
+                    "Signature": sig,
+                    "Gen": gen,
+                    "Position": posi,
+                    "ReadDepth": rd,
+                    "Frequency": freq, 
+                    "Probability": prob,
+                    "Sample_id": name,
+                    "Week": ddir,
+                    "Lineage":lin
+                  })
+
+# filter duplicates 
+df = df.drop_duplicates()
+
+# filter dataframe for searched information
+df_filtered = df.drop(df[(df.ReadDepth < 10) | (df.Frequency < 0.1) | (df.Frequency > 0.9)].index).sort_values(by=['Frequency'], ascending=False).reset_index(drop=True)
+
+# df to show around
+# df = df[["Sample_id", "Gen", "Signature", "Position", "Frequency", "ReadDepth", "Lineage", "Week", "Probability"]]
+# df_show = df.drop(df[(df.ReadDepth < 10) | (df.Frequency < 0.1) | (df.Frequency > 0.9)].index).drop(columns=['Week', 'Probability']).sort_values(by=['Signature']).reset_index(drop=True)
+    
+# filter dataframe for mutations present in more than one week
+# df_wo_unique = df_filtered[df_filtered.duplicated(subset=['Signature'], keep=False)]
+# df_wo_unique = df_wo_unique.sort_values('Signature')
+
+# min_repeat = 10
+# vc = df_filtered["Signature"].value_counts()
+# df_10 = df_filtered[df_filtered["Signature"].isin(vc[vc > min_repeat].index)]
+
+# collect known mutations
+covariants_data = requests.get(
+    "https://raw.githubusercontent.com/hodcroftlab/covariants/master/web/data/clusters.json"
+).json()
+
+mutation_overview = pd.DataFrame()
+
+for i in range(len(covariants_data["clusters"])):
+    strain_name = covariants_data["clusters"][i]["build_name"]
+    if "mutations" in covariants_data["clusters"][i]:
+        for j in range(len(covariants_data["clusters"][i]["mutations"]["nonsynonymous"])):
+            mutation = (
+                str(covariants_data["clusters"][i]["mutations"]["nonsynonymous"][j]["gene"]) + ":" +
+                str(covariants_data["clusters"][i]["mutations"]["nonsynonymous"][j]["left"]) +
+                str(covariants_data["clusters"][i]["mutations"]["nonsynonymous"][j]["pos"]) +
+                str(covariants_data["clusters"][i]["mutations"]["nonsynonymous"][j]["right"])
+            )
+            mutation_overview = pd.concat([mutation_overview, pd.DataFrame({"Signature": [mutation], "Strain": strain_name})], ignore_index=True)
+mutation_overview = mutation_overview.reset_index(drop=True)
+
+# df_show.to_excel("VirusRecombination.xlsx", engine='xlsxwriter', index=False)
+with pd.ExcelWriter('ViralMutations.xlsx', engine='xlsxwriter') as writer:
+    df_filtered.to_excel(writer, sheet_name='filtered', index=False)
+    df.to_excel(writer, sheet_name='all', index=False)
+
+# export filtered mutations without unique mutations
+# df_10.to_csv("wo_unique_mutations.csv", index=False, sep=",")
+
+# export filtered mutations
+df_filtered.to_csv("filtered_mutations.csv", index=False, sep=",")
+
+# export list of all mutations found
+df.to_csv("allmutationsfound.csv", index=False, sep=",")
+
+# export known mutiations overview (complete)
+# mutation_overview.to_csv("known_mutations_overview.csv", index=False)
+
+# comparison with known mutations
+# df_comparison = df_filtered.merge(mutation_overview, left_on='Signature', right_on='Signature', how='inner')
+
+# export found known mutations
+# df_comparison.to_csv("found_mutations_known.csv", index=False, sep=",")
---
+++ workflow/scripts/generate_resistogram.py
@@ -0,0 +1,89 @@
+# Copyright 2022 Katharina Block.
+# Licensed under the BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+# This file may not be copied, modified, or distributed
+# except according to those terms.
+
+# import required packages
+import pandas as pd
+import numpy as np
+import re
+import argparse
+import textwrap
+
+class Formatter(argparse.ArgumentDefaultsHelpFormatter, argparse.MetavarTypeHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    pass
+
+parser = argparse.ArgumentParser(formatter_class=Formatter, 
+                                description=textwrap.dedent('''
+         Please do not mess up this text!
+         --------------------------------
+        Script for estimating efficency of antibodies against SARS-CoV-2 infection in the presence of mutations. 
+        If the patient number is not in the list all known antibodies are efficient. 
+        Predictions are based on the work of Lui et.al (https://doi.org/10.1038/s41586-021-04388-0), who investigated the impact of individual mutations within B.1.1.529 against monoclonal antibodies. 
+        Laboratory data regarding mutation interactions and interdependencies is unavailable. Therefore, the presented algorithm is not able to interfere possible mutation interactions and interdependencies from the data.
+        Since mutation interactions are expected the suggested antibody might be wrong.'''), epilog='Please cite me if you are using this program. Have fun! :)')
+
+parser.add_argument('-p', "--print_string", help="Prints the supplied argument.", default = 'A random string.', nargs='?', const='A random string.', type=str) #works with value after flag given or not 
+parser.add_argument('-k', "--keks", help="Prints the supplied argument.", type=int) #expects value after flag
+parser.add_argument('-z', "--zoom", help="Prints the supplied argument.", action="store_true") #flag sets boolean to True
+parser.add_argument('-w', '--week', help="Selects timespan", default = 'week87', type=str)
+parser.add_argument('-f', '--frequency', help="The frequency, or allel frequency, is the relative frequency of a variant of a nucleic acid at a particular position in a genome.", default = 0.0, nargs='?', const=0.0, type=float)
+parser.add_argument('-d', '--readdepth', help='Number of reads at a particular position in a genome', default = 10, nargs='?', const = 10, type=int)
+args = parser.parse_args()
+
+#retrive args
+print(args)
+week = str(args.week)
+freq = float(args.frequency)
+rd = int(args.readdepth)
+
+# list of mutations effectiv in escaping the antibodies.
+escaping_mutations = pd.read_json('/homes/kblock/scripts/resistogram/mabs.json').set_index('mAbs')
+all_escaping_mutations = [item for sublist in escaping_mutations['Mutation'].tolist() for item in sublist]
+
+# list of factors
+factors = pd.read_csv("/groups/ds/kblock/resistogram/factortab.csv")
+factors = factors[['Mutation', 'S309', 'COV2-2130', 'COV2-2196']].set_index('Mutation')
+
+# list of mutations in samples
+df = pd.read_csv("/groups/ds/kblock/virusrecombination/results/allmutationsfound.csv")
+# filter for tests
+# TODO keep all filenames ?list?
+df = df[(df['Gen'] == 'S') & (df['Week'] == week) & (df['Frequency'] > freq) & (df['ReadDepth'] > rd) & ((df['ReadDepth'] * df['Frequency']) > 1)]
+# filter of all samples containing concerning mutations
+df = df[df['Signature'].str.contains('|'.join(all_escaping_mutations),case=False, na = False)].reset_index(drop=True)
+df = df.drop(columns= ['Position', 'ReadDepth', 'Probability'])
+df = df[['Filename', 'Signature', 'Gen', 'Week', 'Frequency', 'Lineage']].reset_index(drop=True)
+df["ShortSig"] = np.nan
+df["Ineffektiv"] = np.nan
+
+# lists containing mutations 
+medis = escaping_mutations.index.values
+S309 = np.array([escaping_mutations["Mutation"]["S309"]]) #["S:I332","S:T333","S:N334","S:L335","S:C336","S:P337","S:G339","S:E340","S:V341","S:N343","S:A344","S:T345","S:R346","S:N354","S:K356","S:R357","S:I358","S:S359","S:N360","S:C361","S:N440","S:L441","S:R509"]
+AZD1061 = np.array([escaping_mutations["Mutation"]["AZD1061"]]) #["S:T345","S:R346","S:N439","S:N440","S:L441","S:S443","S:K444","S:V445","S:G446","S:G447","S:Y449","S:N450","S:E484","S:F490","S:L492","S:Q493","S:S494","S:P499"]
+AZD8895 = np.array([escaping_mutations["Mutation"]["AZD8895"]]) #["S:L455","S:F456","S:A475","S:G476","S:S477","S:T478","S:P479","S:E484","S:G485","S:F486","S:N487","S:C488","S:Y489","S:Q493"]
+
+#create data structures for loop
+df_sid = pd.DataFrame(columns=['Sample_id', 'Antibody', 'hig_imp_fac', 'Escaping_mutations'])
+df_g = pd.DataFrame(columns=['Sample_id', 'Antibody', 'hig_imp_fac', 'Escaping_mutations'])
+shortsigs =[]
+
+#creation of output file
+for idx, (sid, group) in enumerate(df.groupby('Filename')):
+    #check if there are any antibodies against which there are no mutations? <- Check with Folker
+    group = group.reset_index(drop=True)
+    for ix, sig in enumerate(group['Signature']):
+        short_sig = re.split('(\d+)', sig)[0] + re.split('(\d+)', sig)[1]
+        shortsigs.append(short_sig)
+    i_fac = factors[factors.index.str.contains('|'.join(shortsigs),case = False, na = False)]
+    n_fac = i_fac.applymap(lambda x: (x - (-1000))/(8.6 - (-1000)))
+    ab = n_fac.sum().sort_values(ascending = False)
+    for i, a in enumerate(ab.index):
+        ls = sorted(list(zip(i_fac[ab.index[i]].index, i_fac[ab.index[i]])), key=lambda a: a[1], reverse = True)
+        df_g.at[i, "Escaping_mutations"] = ls
+        df_g.at[i, "Antibody"] = a
+        df_g.at[i, "Sample_id"] = sid
+    df_sid = pd.concat([df_sid, df_g])
+
+print(df_sid)
+df_sid.to_csv('resistogram.csv', index=False)
