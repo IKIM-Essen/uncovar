eaf93cfb3ae66685e2402392207bbd524aa3c3d2
ka-block <katharina.block@uni-due.de>
2023-01-17 11:08:05 +0000

    improvements collection script

--- workflow/scripts/collect_minority_mutations.py
+++ workflow/scripts/collect_minority_mutations.py
@@ -45,160 +45,120 @@ AA_ALPHABET_TRANSLATION = {
     "Thr": "T",
 }
 
-# # create empty lists
-#     "Signature": [],
-#     "Gene": [],
-#     "Position": [],
-#     "ReadDepth": [],
-#     "Frequency": [], 
-#     "Probability": [],
-#     "Sample_id": [],
-#     "Lineage":[]
-
 # create empty lists
-sig=[]
-rd=[]
-freq=[]
-prob=[]
-name=[]
-ddir=[]
-gen=[]
-posi=[]
-lin=[]
+data_dict = {
+    "Signature": [],
+    "Gene": [],
+    "Position": [],
+    "ReadDepth": [],
+    "Frequency": [], 
+    "Probability": [],
+    "Sample_id": [],
+    "Lineage":[],
+}
 
-
-#def bcf_to_df(data_dict, AA_ALPHABET_TRANSLATION):
-# iterate over files in directory
-for file in snakemake.input.bcfs:
-    bcf_in = (pysam.VariantFile(file))
-    filename = path.Path(file).stem
-    lineages = pd.read_csv(snakemake.input.pan_calls, dtype={'Sample': str})
-    idx = list(lineages['Sample']).index(str(filename))
-    # pseudo or scaffold(preferred) from pangolin_calls_per_stage.csv if scaff empty take pseudo
-    lineage = lineages['Scaffolded Seq'].values[idx] if lineages['Scaffolded Seq'].values[idx] != "Not called on" else lineages['Pseudo Seq'].values[idx]
-    # fill lists with data obtained from file
-    # iterate over records
-    for record in bcf_in:
-        pattern_count = str(record).count(':p.')
-        # iterate over annotations in record
-        # as there are multiple annotations per line, which cause problems, check for pattern (Aminoacid/protein annotation) first
-        for ann in record.info["ANN"]:
-            # split string seperated by pipe
-            anno = ann.split("|")
-            # get location
-            loc = anno[3]
-            # get feature_id
-            fea_id = anno[6]
-            # get alteration
-            alt = anno[11]
-            # check for occurence of pattern
-            if pattern_count == 0 and alt != "":
-                continue
-            elif pattern_count != 0 and alt == "":
-                continue
-            elif pattern_count != 0 and alt != "":
-                regex=re.compile(r':p.')
-                alteration = regex.split(alt)[1]
-                # replace %3D and triplet aa code with one letter code
-                for triplet, amino in AA_ALPHABET_TRANSLATION.items():
-                    alteration = alteration.replace("%3D", triplet[0]).replace(triplet, amino)
-                alt = f"{loc}:{alteration}"
-                pos = (record.pos)
-                gene_name = loc
-            elif pattern_count == 0 and alt == "":
-                # if regular name is not available annotate using nucleotides
-                ref=(record.ref)
-                pos=(record.pos)
-                # if clause to find mutation on ORFs
-                if 266 <= pos <= 13468:
-                    gene_name="ORF1a"
-                elif 13468 <= pos <= 21555:
-                    gene_name="ORF1b"
-                elif 21563 <= pos <= 25384:
-                    gene_name="S"
-                elif 25393 <= pos <= 26220:
-                    gene_name="ORF3a"
-                elif 25765 <= pos <= 26220:
-                    gene_name="ORF3b"
-                elif 26245 <= pos <= 26472:
-                    gene_name="E"
-                elif 26523 <= pos <= 27191:
-                    gene_name="M"
-                elif 27202 <= pos <= 27387:
-                    gene_name="ORF6"
-                elif 27394 <= pos <= 27759:
-                    gene_name="ORF7a"
-                elif 27756 <= pos <= 27887:
-                    gene_name="ORF7b"
-                elif 27894 <= pos <= 28259:
-                    gene_name="ORF8"
-                elif 28284 <= pos <= 28577:
-                    gene_name="ORF9a"
-                elif 28734 <= pos <= 28955:
-                    gene_name="ORF9b"
-                elif 28274 <= pos <= 29533:
-                    gene_name="N"
-                elif 29558 <= pos <= 29674:
-                    gene_name="ORF10"
+def bcf_to_df(data_dict, AA_ALPHABET_TRANSLATION):
+    # iterate over files in directory
+    for file in snakemake.input.bcfs:
+        bcf_in = (pysam.VariantFile(file))
+        filename = path.Path(file).stem
+        lineages = pd.read_csv(snakemake.input.pan_calls, dtype={'Sample': str})
+        idx = list(lineages['Sample']).index(str(filename))
+        # pseudo or scaffold(preferred) from pangolin_calls_per_stage.csv if scaff empty take pseudo
+        lineage = lineages['Scaffolded Seq'].values[idx] if lineages['Scaffolded Seq'].values[idx] != "Not called on" else lineages['Pseudo Seq'].values[idx]
+        # fill lists with data obtained from file
+        # iterate over records
+        for record in bcf_in:
+            pattern_count = str(record).count(':p.')
+            # iterate over annotations in record
+            # as there are multiple annotations per line, which cause problems, check for pattern (Aminoacid/protein annotation) first
+            for ann in record.info["ANN"]:
+                # split string seperated by pipe
+                anno = ann.split("|")
+                # get location
+                loc = anno[3]
+                # get feature_id
+                fea_id = anno[6]
+                # get alteration
+                alt = anno[11]
+                # check for occurence of pattern
+                if pattern_count == 0 and alt != "":
+                    continue
+                elif pattern_count != 0 and alt == "":
+                    continue
+                elif pattern_count != 0 and alt != "":
+                    regex=re.compile(r':p.')
+                    alteration = regex.split(alt)[1]
+                    # replace %3D and triplet aa code with one letter code
+                    for triplet, amino in AA_ALPHABET_TRANSLATION.items():
+                        alteration = alteration.replace("%3D", triplet[0]).replace(triplet, amino)
+                    alt = f"{loc}:{alteration}"
+                    pos = (record.pos)
+                    gene_name = loc
+                elif pattern_count == 0 and alt == "":
+                    # if regular name is not available annotate using nucleotides
+                    ref=(record.ref)
+                    pos=(record.pos)
+                    # if clause to find mutation on ORFs
+                    if 266 <= pos <= 13468:
+                        gene_name="ORF1a"
+                    elif 13468 <= pos <= 21555:
+                        gene_name="ORF1b"
+                    elif 21563 <= pos <= 25384:
+                        gene_name="S"
+                    elif 25393 <= pos <= 26220:
+                        gene_name="ORF3a"
+                    elif 25765 <= pos <= 26220:
+                        gene_name="ORF3b"
+                    elif 26245 <= pos <= 26472:
+                        gene_name="E"
+                    elif 26523 <= pos <= 27191:
+                        gene_name="M"
+                    elif 27202 <= pos <= 27387:
+                        gene_name="ORF6"
+                    elif 27394 <= pos <= 27759:
+                        gene_name="ORF7a"
+                    elif 27756 <= pos <= 27887:
+                        gene_name="ORF7b"
+                    elif 27894 <= pos <= 28259:
+                        gene_name="ORF8"
+                    elif 28284 <= pos <= 28577:
+                        gene_name="ORF9a"
+                    elif 28734 <= pos <= 28955:
+                        gene_name="ORF9b"
+                    elif 28274 <= pos <= 29533:
+                        gene_name="N"
+                    elif 29558 <= pos <= 29674:
+                        gene_name="ORF10"
+                    else:
+                        gene_name="none"
+                    alts=(record.alts[0])
+                    alt=f"{ref}{pos}{alts}"
                 else:
-                    gene_name="none"
-                alts=(record.alts[0])
-                alt=f"{ref}{pos}{alts}"
-            else:
-                raise Exception("Something unexpected occured in this record:", str(record), file)
-            # add results in lists
-            sig.append(alt)
-            # position
-            posi.append(pos)
-            # ORF
-            gen.append(gene_name)
-            # read depth
-            rd.append(record.samples[0]["DP"])
-            # frequency
-            freq.append(record.samples[0]["AF"][0])
-            # probability
-            prob.append(1.0 - (phred_to_prob(record.info["PROB_ABSENT"][0]) + phred_to_prob(record.info["PROB_ARTIFACT"][0])))
-            # filename
-            name.append(filename)
-            # lineage
-            lin.append(lineage)
-            # # add results in lists
-            # data_dict["Signature"].append(alt)
-            # # position
-            # data_dict["Position"].append(pos)
-            # # ORF
-            # data_dict["Gene"].append(gene_name)
-            # # read depth
-            # data_dict["ReadDepth"].append(record.samples[0]["DP"])
-            # # frequency
-            # data_dict["Frequency"].append(record.samples[0]["AF"][0])
-            # # probability
-            # data_dict["Probability"].append(1.0 - (phred_to_prob(record.info["PROB_ABSENT"][0]) + phred_to_prob(record.info["PROB_ARTIFACT"][0])))
-            # # filename
-            # data_dict["Sample_id"].append(filename)
-            # # lineage
-            # data_dict["Lineage"].append(lineage)
+                    raise Exception("Something unexpected occured in this record:", str(record), file)
+                # add results in lists
+                data_dict["Signature"].append(alt)
+                # position
+                data_dict["Position"].append(pos)
+                # ORF
+                data_dict["Gene"].append(gene_name)
+                # read depth
+                data_dict["ReadDepth"].append(record.samples[0]["DP"])
+                # frequency
+                data_dict["Frequency"].append(record.samples[0]["AF"][0])
+                # probability
+                data_dict["Probability"].append(1.0 - (phred_to_prob(record.info["PROB_ABSENT"][0]) + phred_to_prob(record.info["PROB_ARTIFACT"][0])))
+                # filename
+                data_dict["Sample_id"].append(filename)
+                # lineage
+                data_dict["Lineage"].append(lineage)
+    # create dataframe
+    df = pd.DataFrame().from_dict(data_dict)
+    # filter duplicates 
+    df = df.drop_duplicates()
+    return(df)
 
-# create dataframe
-df= pd.DataFrame({
-                    "Signature": sig,
-                    "Gen": gen,
-                    "Position": posi,
-                    "ReadDepth": rd,
-                    "Frequency": freq, 
-                    "Probability": prob,
-                    "Sample_id": name,
-                    "Lineage":lin
-                  })
-# create dataframe
-#df = pd.DataFrame()
-#.from_dict(data_dict)
-    #return(df)
-
-#df = bcf_to_df(data_dict, AA_ALPHABET_TRANSLATION)
-
-# filter duplicates 
-df = df.drop_duplicates()
+df = bcf_to_df(data_dict, AA_ALPHABET_TRANSLATION)
 
 # export list of all mutations found
 df.to_csv(snakemake.output[0], index=False, sep=",")
